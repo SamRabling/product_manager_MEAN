6a7b8b3807a3cc79bebbba64685528778a63eb6e
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-17 13:11:01 -0700

    done with prod list component

--- AngularApp/src/app/prod-list/prod-list.component.ts
+++ AngularApp/src/app/prod-list/prod-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-prod-list',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prod-list.component.css']
 })
 export class ProdListComponent implements OnInit {
+  products = [];
 
-  constructor() { }
+  constructor(private _httpService: HttpService, private _router: Router) { }
 
   ngOnInit() {
+    this.allProducts();
   }
 
+  allProducts() {
+    const observable = this._httpService.showMeProducts();
+    observable.subscribe( data => {
+      this.products = data ['data'];
+    });
+  }
+
+  removeProd(id: string) {
+    const observable = this._httpService.deleteProduct(id, this.products);
+    observable.subscribe(data => {
+      this.allProducts();
+    });
+  }
 }

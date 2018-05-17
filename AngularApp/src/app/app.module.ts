602783d12d0d0d4f4de9541d2a1ef53ac91723c8
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-17 11:43:00 -0700

    setting up components

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import { ProdListComponent } from './prod-list/prod-list.component';
 import { ProdNewComponent } from './prod-new/prod-new.component';
+import { ProdEditComponent } from './prod-edit/prod-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
     ProdListComponent,
-    ProdNewComponent
+    ProdNewComponent,
+    ProdEditComponent
   ],
   imports: [
     BrowserModule,

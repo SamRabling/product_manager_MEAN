24dabdc012128391587e50981dc0fafd216606a3
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-17 12:29:27 -0700

    setting up components

--- AngularApp/src/app/app-routing.module.ts
+++ AngularApp/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { ProdListComponent } from './prod-list/prod-list.component';
 import { ProdNewComponent } from './prod-new/prod-new.component';
+import { ProdEditComponent } from './prod-edit/prod-edit.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: IndexComponent },
   { path: 'products/list', component: ProdListComponent },
-  { path: 'products/new', component: ProdNewComponent }
+  { path: 'products/new', component: ProdNewComponent },
+  { path: 'products/edit/:id', component: ProdEditComponent}
 ];
 
 @NgModule({

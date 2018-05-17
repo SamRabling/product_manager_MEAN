8f65c8299ad9c59d35bd65c92812e0507d221981
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-17 13:16:31 -0700

    done with prod edit component

--- AngularApp/src/app/prod-edit/prod-edit.component.ts
+++ AngularApp/src/app/prod-edit/prod-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-prod-edit',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prod-edit.component.css']
 })
 export class ProdEditComponent implements OnInit {
+  edit_prod = {title: '', price: '', img: ''};
+  id: string;
+  prod_params: any;
 
-  constructor() { }
+  constructor(
+    private _httpService: HttpService,
+    private _router: Router,
+    private _route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.edit_prod = { title: '', price: '', img: '' };
+    this.prod_params = this._route.params.subscribe(params => this.id = params['id']);
+  }
+
+  oneProduct(id: string) {
+    const obsevable = this._httpService.oneProduct(id);
+    obsevable.subscribe(data => this.edit_prod = data['data']);
+  }
+
+  editProduct() {
+    const obsevable = this._httpService.editProduct(this.id, this.edit_prod);
+    obsevable.subscribe(data => {
+      this.edit_prod = { title: '', price: '', img: '' };
+      this._router.navigate(['/']);
+    });
   }
 
 }
